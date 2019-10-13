e4a481494d68698ab175f69b0526da1772152447
ElatedEatrs <51804250+ElatedEatrs@users.noreply.github.com>
2019-10-13 09:47:05 -0700

    Making stuff consistent with the compilable build

---
+++ NewSF64Toolkit/AboutControl.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace NewSF64Toolkit
+{
+    partial class AboutControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblBy = new System.Windows.Forms.Label();
+            this.lblOrig = new System.Windows.Forms.Label();
+            this.lblOther = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblCRC = new System.Windows.Forms.Label();
+            this.lblParasyte = new System.Windows.Forms.Label();
+            this.lblUCON = new System.Windows.Forms.Label();
+            this.lblHyper = new System.Windows.Forms.Label();
+            this.lblMIO0 = new System.Windows.Forms.Label();
+            this.lblfku = new System.Windows.Forms.Label();
+            this.lblSF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(264, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "New Star Fox 64 Toolkit";
+            // 
+            // lblBy
+            // 
+            this.lblBy.AutoSize = true;
+            this.lblBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblBy.Location = new System.Drawing.Point(14, 49);
+            this.lblBy.Name = "lblBy";
+            this.lblBy.Size = new System.Drawing.Size(150, 13);
+            this.lblBy.TabIndex = 1;
+            this.lblBy.Text = "A modding tool by mib_f8sm9c";
+            // 
+            // lblOrig
+            // 
+            this.lblOrig.AutoSize = true;
+            this.lblOrig.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblOrig.Location = new System.Drawing.Point(14, 82);
+            this.lblOrig.Name = "lblOrig";
+            this.lblOrig.Size = new System.Drawing.Size(225, 13);
+            this.lblOrig.TabIndex = 2;
+            this.lblOrig.Text = "Based off original SF64Toolkit by XDanielDZD";
+            // 
+            // lblOther
+            // 
+            this.lblOther.AutoSize = true;
+            this.lblOther.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblOther.Location = new System.Drawing.Point(14, 110);
+            this.lblOther.Name = "lblOther";
+            this.lblOther.Size = new System.Drawing.Size(70, 13);
+            this.lblOther.TabIndex = 3;
+            this.lblOther.Text = "Other credits:";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(275, 12);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(67, 25);
+            this.lblVersion.TabIndex = 4;
+            this.lblVersion.Text = "V.0.0";
+            // 
+            // lblCRC
+            // 
+            this.lblCRC.AutoSize = true;
+            this.lblCRC.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblCRC.Location = new System.Drawing.Point(32, 173);
+            this.lblCRC.Name = "lblCRC";
+            this.lblCRC.Size = new System.Drawing.Size(82, 13);
+            this.lblCRC.TabIndex = 5;
+            this.lblCRC.Text = "N64 CRC code:";
+            // 
+            // lblParasyte
+            // 
+            this.lblParasyte.AutoSize = true;
+            this.lblParasyte.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblParasyte.Location = new System.Drawing.Point(51, 191);
+            this.lblParasyte.Name = "lblParasyte";
+            this.lblParasyte.Size = new System.Drawing.Size(48, 13);
+            this.lblParasyte.TabIndex = 6;
+            this.lblParasyte.Text = "Parasyte";
+            // 
+            // lblUCON
+            // 
+            this.lblUCON.AutoSize = true;
+            this.lblUCON.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblUCON.Location = new System.Drawing.Point(51, 210);
+            this.lblUCON.Name = "lblUCON";
+            this.lblUCON.Size = new System.Drawing.Size(48, 13);
+            this.lblUCON.TabIndex = 7;
+            this.lblUCON.Text = "uCON64";
+            // 
+            // lblHyper
+            // 
+            this.lblHyper.AutoSize = true;
+            this.lblHyper.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblHyper.Location = new System.Drawing.Point(51, 255);
+            this.lblHyper.Name = "lblHyper";
+            this.lblHyper.Size = new System.Drawing.Size(70, 13);
+            this.lblHyper.TabIndex = 9;
+            this.lblHyper.Text = "HyperHacker";
+            // 
+            // lblMIO0
+            // 
+            this.lblMIO0.AutoSize = true;
+            this.lblMIO0.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblMIO0.Location = new System.Drawing.Point(32, 236);
+            this.lblMIO0.Name = "lblMIO0";
+            this.lblMIO0.Size = new System.Drawing.Size(110, 13);
+            this.lblMIO0.TabIndex = 8;
+            this.lblMIO0.Text = "MIO0 decompression:";
+            // 
+            // lblfku
+            // 
+            this.lblfku.AutoSize = true;
+            this.lblfku.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblfku.Location = new System.Drawing.Point(51, 152);
+            this.lblfku.Name = "lblfku";
+            this.lblfku.Size = new System.Drawing.Size(38, 13);
+            this.lblfku.TabIndex = 11;
+            this.lblfku.Text = "fkualol";
+            // 
+            // lblSF
+            // 
+            this.lblSF.AutoSize = true;
+            this.lblSF.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.lblSF.Location = new System.Drawing.Point(32, 132);
+            this.lblSF.Name = "lblSF";
+            this.lblSF.Size = new System.Drawing.Size(112, 13);
+            this.lblSF.TabIndex = 10;
+            this.lblSF.Text = "SF64 level format info:";
+            // 
+            // AboutControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblfku);
+            this.Controls.Add(this.lblSF);
+            this.Controls.Add(this.lblHyper);
+            this.Controls.Add(this.lblMIO0);
+            this.Controls.Add(this.lblUCON);
+            this.Controls.Add(this.lblParasyte);
+            this.Controls.Add(this.lblCRC);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.lblOther);
+            this.Controls.Add(this.lblOrig);
+            this.Controls.Add(this.lblBy);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AboutControl";
+            this.Size = new System.Drawing.Size(396, 290);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblBy;
+        private System.Windows.Forms.Label lblOrig;
+        private System.Windows.Forms.Label lblOther;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblCRC;
+        private System.Windows.Forms.Label lblParasyte;
+        private System.Windows.Forms.Label lblUCON;
+        private System.Windows.Forms.Label lblHyper;
+        private System.Windows.Forms.Label lblMIO0;
+        private System.Windows.Forms.Label lblfku;
+        private System.Windows.Forms.Label lblSF;
+    }
+}
